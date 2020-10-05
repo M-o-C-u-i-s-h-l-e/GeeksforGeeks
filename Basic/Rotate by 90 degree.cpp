499bb9a22def454b08bbaedfd1b1585b91537118
Genie <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2020-10-05 22:07:59 +0530

    Create Rotate by 90 degree.cpp

---
+++ Basic/Rotate by 90 degree.cpp
@@ -0,0 +1,12 @@
+void rotate(vector<vector<int> >& matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < (n>>1); ++i) {
+        for (int j = i; j < n-i-1; ++j) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][n-1-i];
+            matrix[j][n-1-i] = matrix[n-1-i][n-1-j];
+            matrix[n-1-i][n-1-j] = matrix[n-1-j][i];
+            matrix[n-1-j][i] = temp;
+        }
+    }
+}
