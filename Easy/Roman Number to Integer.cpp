e415fe54810b642bc5e765e49e7bedced3812ad0
Genie <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2020-10-05 23:17:43 +0530

    Update Roman Number to Integer.cpp

--- Easy/Roman Number to Integer.cpp
+++ Easy/Roman Number to Integer.cpp
@@ -1,30 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int value(char c) {
-    if (c == 'I')
-        return 1;
-    if (c == 'V')
-        return 5;
-    if (c == 'X')
-        return 10;
-    if (c == 'L')
-        return 50;
-    if (c == 'C')
-        return 100;
-    if (c == 'D')
-        return 500;
-    if (c == 'M')
-        return 1000;
-    return 0;
-}
+map<char, int> mp = {
+    {'I', 1},
+    {'V', 5},
+    {'X', 10},
+    {'L', 50},
+    {'C', 100},
+    {'D', 500},
+    {'M', 1000}
+};
 
 int romanToDecimal(string str) {
     int res = 0;
     for (int i = 0; i < str.size(); ++i) {
-        int temp1 = value(str[i]);
+        int temp1 = mp[str[i]];
         if (i+1 < str.size()) {
-            int temp2 = value(str[i+1]);
+            int temp2 = mp[str[i+1]];
             if (temp1 >= temp2) {
                 res += temp1;
             } else {
